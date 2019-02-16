63004675de2e4aa5c4e60417a467935cce2b34cd
whh <2389796069@qq.com>
2019-02-16 09:50:47 +0800

    添加bulid

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var minCss = require('gulp-clean-css');
 var uglify = require("gulp-uglify");
+var webserver = require('gulp-webserver');
+var concat = require('gulp-concat');
 
 //编译sass压缩css
 gulp.task("sass", function() {
@@ -10,9 +12,28 @@ gulp.task("sass", function() {
             .pipe(minCss())
             .pipe(gulp.dest("./src/css"))
     })
-    //压缩js
+    //压缩js  合并js文件
 gulp.task("uglify", function() {
+        return gulp.src('./src/js/*.js')
+            .pipe(uglify())
+            .pipe(concat('all.js'))
+            .pipe(gulp.dest("./dist/js"))
+    })
+    //起服务
+gulp.task('webserver', function() {
+        return gulp.src("src")
+            .pipe(webserver({
+                port: 9090,
+                open: true,
+                livereload: true,
+            }))
+    })
+    //创建build任务
+gulp.task('bulidcss', function() {
+    return gulp.src('./src/css/*.css')
+        .pipe(gulp.dest('./bulid/css'))
+})
+gulp.task('bulidjs', function() {
     return gulp.src('./src/js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest("./dist/js"))
+        .pipe(gulp.dest('./bulid/js'))
 })
