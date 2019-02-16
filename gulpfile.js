0da861edbd88699e5bd5e1d0ff93ad0a7609ffef
whh <2389796069@qq.com>
2019-02-16 09:27:16 +0800

    提交压缩js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var minCss = require('gulp-clean-css');
+var uglify = require("gulp-uglify");
+
 //编译sass压缩css
 gulp.task("sass", function() {
-    return gulp.src('./src/scss/*.scss')
-        .pipe(sass())
-        .pipe(minCss())
-        .pipe(gulp.dest("./src/css"))
+        return gulp.src('./src/scss/*.scss')
+            .pipe(sass())
+            .pipe(minCss())
+            .pipe(gulp.dest("./src/css"))
+    })
+    //压缩js
+gulp.task("uglify", function() {
+    return gulp.src('./src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest("./dist/js"))
 })
